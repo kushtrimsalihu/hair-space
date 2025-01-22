e5f35f3e34e1209c47eb1eae242b23cebc538313
kushtrimsalihu <kushttrim.salihu@gmail.com>
2025-01-22 23:17:16 +0100

    style fixes

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -5,23 +5,24 @@ import { Head } from '@inertiajs/vue3';
 
 <template>
     <Head title="Dashboard" />
-
     <AuthenticatedLayout>
         <template #header>
             <h2
-                class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200"
-            >
+                class="text-xl font-semibold leading-tight text-white">
                 Dashboard
             </h2>
         </template>
 
         <div class="py-12">
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="ml-auto max-w-3xl sm:px-6 lg:px-8">
                 <div
                     class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800"
                 >
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        You're logged in!
+                    <div class="p-6 text-gray-900 dark:text-gray-100 border-b border-gray-300">
+                        <p class="text-lg">Hello,
+                       {{ $page.props.auth.user.name }}, you're logged in!
+                       </p>
+                       <p class="text-white font-bold">Welcome in Hair Space! </p>
                     </div>
                 </div>
             </div>
